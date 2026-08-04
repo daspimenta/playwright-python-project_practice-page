cfb0acbff0e4ca2d0a161477760dcb99bd63e014
daspimenta <daspimenta@hotmail.com>
2026-08-04 17:18:15 -0300

    Commit 5 Tests

--- tests/test_all_locators.py
+++ tests/test_all_locators.py
@@ -4,9 +4,13 @@ from playwright.sync_api import Page, expect
 def test_all_locators(page: Page):
     page.goto("https://leogcarvalho.github.io/test-automation-practice/playwright-locators.html")
 
-# get_by_role
+# get_by_role    
     expect(page.get_by_role("button", name="Explicit Role Button")).to_be_visible()
-    expect(page.get_by_role("link", name="Explicit Role Link")).to_be_visible()
+    expect(page.get_by_role("link", name="Explicit Role Link")).to_be_visible()    
+    expect(page.get_by_role("img", name="robot icon")).to_be_visible()    
+    expect(page.get_by_role("button", name="Implicit Button")).to_be_visible()    
+    expect(page.get_by_role("link", name="Implicit Link")).to_be_visible()
     
+
 
 
